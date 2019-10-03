c3a2d169d31b6ff15f402c8ae2200bbce3da4180
Eduardo de Leon <eddeleon@microsoft.com>
2019-10-02 22:37:33 -0400

    Add helper for dataset client getter

--- tests/service/dataset/test_smoke.py
+++ tests/service/dataset/test_smoke.py
@@ -2,12 +2,11 @@ import json
 
 import pytest
 
-from burdock.restclient.models.dataset_read_request import DatasetReadRequest
+from burdock.client import get_dataset_client
 
-@pytest.mark.parametrize("dataset_id", ["0"])
-def test_read_local_csv(client, dataset_id):
-    read_request = DatasetReadRequest(dataset_id=dataset_id)
-    dataset_document = client.datasetread(read_request)
+@pytest.mark.parametrize("dataset_name", ["example"])
+def test_read_local_csv(dataset_client, dataset_name):
+    dataset_document = dataset_client.read(dataset_name, None)
     local_csv_details = dataset_document.csv_details
 
     with open(local_csv_details.local_path, "r") as stream:
