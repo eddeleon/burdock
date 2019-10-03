b499514fd33cfbfcd0db157f727fe59ec4363825
Eduardo de Leon <eddeleon@microsoft.com>
2019-10-02 23:10:21 -0400

    Add adapters and csv adapter test

--- tests/service/dataset/test_smoke.py
+++ tests/service/dataset/test_smoke.py
@@ -1,8 +1,10 @@
 import json
+import pandas as pd
 
 import pytest
 
 from burdock.client import get_dataset_client
+from burdock.data.adapters import load_dataset
 
 @pytest.mark.parametrize("dataset_name", ["example"])
 def test_read_local_csv(dataset_client, dataset_name):
@@ -12,4 +14,11 @@ def test_read_local_csv(dataset_client, dataset_name):
     with open(local_csv_details.local_path, "r") as stream:
         text = stream.read()
         assert len(text) > 0
+
+
+@pytest.mark.parametrize("dataset_name", ["example"])
+def test_load_local_csv(dataset_client, dataset_name):
+    dataset_document = dataset_client.read(dataset_name, None)
+    df = load_dataset(dataset_document)
+    isinstance(df, pd.pandas.core.frame.DataFrame)
 
